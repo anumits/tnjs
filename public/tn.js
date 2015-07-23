5e9c9dc9dab177000447d5673f42d93c5c80dd9f
Kevin Deisz <kevin.deisz@gmail.com>
2015-07-23 08:31:33 -0400

    adding ajax functionality

--- public/tn.js
+++ public/tn.js
@@ -5,3 +5,16 @@
 // animation code
 
 // ajax code
+window.$ = {};
+$.ajax = function(url, method, callback) {
+	var request = new XMLHttpRequest();
+	request.onreadystatechange = function() {
+		var DONE = this.DONE || 4;
+		if(this.readyState === DONE){
+			callback(this.responseText);
+		}
+	};
+	
+	request.open(method.toUpperCase(), url);
+	request.send();
+};
